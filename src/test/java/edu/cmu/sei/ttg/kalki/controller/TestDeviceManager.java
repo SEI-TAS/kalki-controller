ac406e7985078421ddfe0424e530aee9a367a81d
Josh Schlichting <jrs322@case.edu>
2019-06-28 11:37:12 -0400

    Added Test Classes

---
+++ src/test/java/edu/cmu/sei/ttg/kalki/controller/TestDeviceManager.java
@@ -0,0 +1,49 @@
+package edu.cmu.sei.ttg.kalki.controller;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDeviceManager {
+
+    DeviceManager mainManager;
+
+    @Before
+    public void testManagerInit(){
+        mainManager = new DeviceManager();
+    }
+    @Test
+    public void testPHLEDeviceSearch(){
+        mainManager.pushNewPHLE("phle00", 0);
+        mainManager.pushNewPHLE("phle01", 1);
+        assertEquals("phle00", mainManager.queryForPHLE("phle00").getName());
+        assertEquals("phle01", mainManager.queryForPHLE("phle01").getName());
+        assertEquals("empty", mainManager.queryForPHLE("phle02").getName());
+    }
+    @Test
+    public void testUNTSDeviceSearch(){
+        mainManager.pushNewUNTS("unts00", 0);
+        mainManager.pushNewUNTS("unts01", 1);
+        assertEquals("unts00", mainManager.queryForUNTS("unts00").getName());
+        assertEquals("unts01", mainManager.queryForUNTS("unts01").getName());
+        assertEquals("empty", mainManager.queryForUNTS("unts02").getName());
+    }
+    @Test
+    public void testDLCDeviceSearch(){
+        mainManager.pushNewDLC("dlc00", 0);
+        mainManager.pushNewDLC("dlc01", 1);
+        assertEquals("dlc00", mainManager.queryForDLC("dlc00").getName());
+        assertEquals("dlc01", mainManager.queryForDLC("dlc01").getName());
+        assertEquals("empty", mainManager.queryForDLC("dlc02").getName());
+    }
+    @Test
+    public void testWEMODeviceSearch(){
+        mainManager.pushNewWEMO("wemo00", 0);
+        mainManager.pushNewWEMO("wemo01", 1);
+        assertEquals("wemo00", mainManager.queryForWEMO("wemo00").getName());
+        assertEquals("wemo01", mainManager.queryForWEMO("wemo01").getName());
+        assertEquals("empty", mainManager.queryForWEMO("wemo02").getName());
+    }
+
+
+}
