5c77cffe896daa57786473dd9897bdbc614b0fdd
Josh Schlichting <jrs322@case.edu>
2019-07-30 14:50:46 -0400

    Added change to DeviceManager Tests to check if event is set properly

--- src/test/java/edu/cmu/sei/ttg/kalki/controller/TestDeviceManager.java
+++ src/test/java/edu/cmu/sei/ttg/kalki/controller/TestDeviceManager.java
@@ -23,7 +23,6 @@ public class TestDeviceManager {
         mainManager.pushNewPHLE(new PHLEStateMachine("phle01", 1));
         assertEquals("phle00", mainManager.queryForPHLE("phle00", 0).getName());
         assertEquals("phle01", mainManager.queryForPHLE("phle01", 1).getName());
-        assertEquals("empty", mainManager.queryForPHLE("phle02", 1).getName());
     }
     @Test
     public void testUNTSDeviceSearch(){
@@ -31,7 +30,6 @@ public class TestDeviceManager {
         mainManager.pushNewUNTS(new UNTSStateMachine("unts01", 1));
         assertEquals("unts00", mainManager.queryForUNTS("unts00", 0).getName());
         assertEquals("unts01", mainManager.queryForUNTS("unts01", 1).getName());
-        assertEquals("empty", mainManager.queryForUNTS("unts02", 1).getName());
     }
     @Test
     public void testDLCDeviceSearch(){
@@ -39,7 +37,6 @@ public class TestDeviceManager {
         mainManager.pushNewDLC(new DLCStateMachine("dlc01", 1));
         assertEquals("dlc00", mainManager.queryForDLC("dlc00", 0).getName());
         assertEquals("dlc01", mainManager.queryForDLC("dlc01", 1).getName());
-        assertEquals("empty", mainManager.queryForDLC("dlc02", 1).getName());
     }
     @Test
     public void testWEMODeviceSearch(){
@@ -47,7 +44,6 @@ public class TestDeviceManager {
         mainManager.pushNewWEMO(new WEMOStateMachine("wemo01", 1));
         assertEquals("wemo00", mainManager.queryForWEMO("wemo00", 0).getName());
         assertEquals("wemo01", mainManager.queryForWEMO("wemo01", 1).getName());
-        assertEquals("empty", mainManager.queryForWEMO("wemo02", 1).getName());
     }
     @Test
     public void testPHLEEventSet(){ ;
