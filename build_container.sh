23e6632ccaa3b26010b175c4067ff67f38a612f9
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-12 23:45:50 -0400

    Removing explicit test db call

--- build_container.sh
+++ build_container.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-docker stop kalki-postgres-test
 docker run -p 5433:5432 --rm -d --name kalki-postgres-test kalki/kalki-postgres-test
-docker build --network=host -t kalki/kalki-main-controller .
-docker stop kalki-postgres-test
